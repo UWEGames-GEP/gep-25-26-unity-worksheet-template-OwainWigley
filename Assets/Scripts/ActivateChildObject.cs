c11511040151ee463fd1a2fa2954692512dc7cf8
Owain Wigley <Owain2.Wigley@live.uwe.ac.uk>
2026-04-17 04:26:30 +0100

    unity

--- Assets/Scripts/ActivateChildObject.cs
+++ Assets/Scripts/ActivateChildObject.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 
 public class ActivateChildObject : MonoBehaviour
 {
-    public int childIndex;
-
     // activates child of game object on entering trigger
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player")) 
         {
-            transform.GetChild(childIndex).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(true);
         }
     }
 
@@ -18,7 +16,7 @@ public class ActivateChildObject : MonoBehaviour
     {
         if (collider.CompareTag("Player"))
         {
-            transform.GetChild(childIndex).gameObject.SetActive(false);
+            transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
